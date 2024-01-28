3d4c51cc922c9243ab0387a17801ae2e97111a6c
Wardah-RL <wardah.rose123@gmail.com>
2024-01-28 15:27:17 +0700

    move file

---
+++ DirectoryApp/Interface/DirectoryInterface.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DirectoryApp.Interface
+{
+  public interface DirectoryInterface
+  {
+    void Scanning(string directory);
+    void ListDirectory(string directory);
+    void Suggest(string directory);
+    //List<ListFilesReturn> ListDirectorys(string directory);
+    //List<ListFilesReturn> ListFiles(string directory);
+    //List<ListFilesReturn> GetAllDirectory(string directory);
+    public void GetDirectory(string directory, string input);
+  }
+}
---
+++ DirectoryApp/Model/ListFilesReturn.cs
@@ -0,0 +1,16 @@
+﻿using DirectoryApp.Enum;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DirectoryApp.Model
+{
+  public class ListFilesReturn
+  {
+    public string file { get; set; }
+    public long size { get; set; }
+    public TypeFile type { get; set; }
+  }
+}
