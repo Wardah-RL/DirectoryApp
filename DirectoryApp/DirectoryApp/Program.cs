8628c65b08c399612ec5128099075cb1e46ac0fa
Wardah-RL <98136876+Wardah-RL@users.noreply.github.com>
2024-01-28 15:24:17 +0700

    upload directory app

---
+++ DirectoryApp/DirectoryApp/Program.cs
@@ -0,0 +1,28 @@
+﻿using DirectoryApp.Service;
+
+public class Program
+{
+  static void Main()
+  {
+    MenuDirectory();
+  }
+
+  public static void MenuDirectory()
+  {
+    Console.Write("cd ");
+    var directory = Console.ReadLine();
+    Menu(directory);
+  }
+  public static void Menu(string directory)
+  {
+    Console.Write("[0] Input Directory");
+    Console.Write("[1] Scan");
+    Console.Write("[2] Suggest");
+    Console.Write("[3] Is");
+    Console.WriteLine("[4] Exit");
+    var index = Console.ReadLine();
+    DirectoryService Directory = new DirectoryService();
+    Directory.GetDirectory(directory, index);
+  }
+
+}
